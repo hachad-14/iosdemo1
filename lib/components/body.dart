2611166ecf0196a78101c0184d9edd147aa26d2a
hachad-14 <hachadsaad14@gmail.com>
2020-11-20 20:52:10 +0100

    test mon gatéeee

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,94 +1,96 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:device_apps/device_apps.dart';
 import 'package:iosdemo/constants.dart';
+
+import 'header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                 left: kDefaultPadding,
-                ),
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  )
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      "Wesh Lazez",
-                      style: Theme.of(context).textTheme.headline5.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Spacer(),
-                    Image.asset("assets/images/logo.png")
-                  ],
-                )
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0,10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      )
-                    ]
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Chercher",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            // blala$
-                            //suffixIcon: 
-                                //SvgPicture.asset("assets/icons/search.svg"),
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset("assets/icons/search.svg"),
-                    ],
-                  ),
-                )
-              )
-            ],
+    return SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: "Caméra",
+            press: () {
+              DeviceApps.openApp('com.apple.camera');
+              },
           ),
-        )
-      ],
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key key,
+    this.title,
+    this.press,
+  }) : super(key: key);
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget> [
+          TitleWithCustomUnderline(text: title),
+          Spacer(),
+          FlatButton(
+            color: kPrimaryColor,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
+            ),
+            onPressed: press,
+            child: Text(
+              "Ouvrir",
+              style: TextStyle(color: Colors.white),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2)
+            ),
+          ),
+        ]
+      ),
     );
   }
 }
